5c638fdb836db19ef6399cb79d4c08070393791b
COUTINHO ALVES <couto.alves@outlook.com>
2026-05-04 17:22:14 -0300

    primeiro

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+// filepath: prisma.ts
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const prismaClient = new PrismaClient({
+    adapter: new PrismaPg({
+        connectionString: process.env.DATABASE_URL,
+    }),
+});
+
+export { prismaClient as prisma };
